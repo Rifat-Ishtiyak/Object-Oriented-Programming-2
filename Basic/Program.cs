4dfcf67c47fd0ead1e1a2199dec28945aee252c1
ishtiyak98 <rifatishtiyak@gmail.com>
2023-11-13 01:36:09 +0600

    basic practice

---
+++ Basic/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Basic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string Name = "Rifat";
+            Console.WriteLine(Name);
+
+            //tarnary operator
+            const string ab = Name == "Rifat" ? "OK" : "NOO";
+            Console.WriteLine(ab);
+
+            //Array
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+        }
+    }
+}
